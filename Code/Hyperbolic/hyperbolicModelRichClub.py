7e695bc18942b335c6579d3307b6b92e156ab7a2
matecsigi <csimate5@gmail.com>
2018-02-14 16:31:41 +0100

    Using the exact formula for hyperbolic distance

--- Code/Hyperbolic/hyperbolicModelRichClub.py
+++ Code/Hyperbolic/hyperbolicModelRichClub.py
@@ -1,6 +1,6 @@
 import sys
 import networkx as nx
-import igraph
+#import igraph
 import math
 from mpmath import *
 import random
@@ -14,16 +14,16 @@ def hyperbolicDistance(G, node1, node2, beta=1):
     """Calculates the distance between two points in hyperbolic space."""
     angle1 = G.node[node1]["angle"]
     angle2 = G.node[node2]["angle"]
-    radius1 = G.node[node1]["radius"]
-    radius2 = G.node[node2]["radius"]
-    deltaAngle = abs(angle1-angle2)
-    return abs(radius1+radius2+math.log(deltaAngle/float(2)))
-
+    r1 = G.node[node1]["radius"]
+    r2 = G.node[node2]["radius"]
+    delta = abs(angle1-angle2)
+    #return abs(r1+r2+math.log(sin(delta/float(2))))
+    return acosh(cosh(r1)*cosh(r2)-sinh(r1)*sinh(r2)*cos(delta))
+    
 def addNode(G, id):
     """Adds a node to the network assigning coordinates to it."""
     G.add_node(id)
     G.node[id]["radius"] = hyperbolicRadius(t+1)
-    print id, "->", G.node[id]["radius"]
     G.node[id]["angle"] = random.uniform(0, 2*math.pi)
 
 def kClosest(G, id, k):
@@ -33,16 +33,16 @@ def kClosest(G, id, k):
         dists.append(hyperbolicDistance(G, id, nodesPresent))
     return numpy.argsort(dists)[0:min(id, k)]
 
-def saveGML(G):
-    """Saves the network in a gml format."""
-    g = igraph.Graph(directed=False)
-    g.add_vertices(G.nodes())
-    for node in G.nodes():
-        g.vs[node]["radius"] = G.node[node]["radius"]
-        g.vs[node]["angle"] = G.node[node]["angle"]
-        g.add_edges(G.edges())
-
-    g.save("hyperbolicModel"+"rand"+str(random.randrange(1, 100))+".gml")
+#def saveGML(G):
+#    """Saves the network in a gml format."""
+#    g = igraph.Graph(directed=False)
+#    g.add_vertices(G.nodes())
+#    for node in G.nodes():
+#        g.vs[node]["radius"] = G.node[node]["radius"]
+#        g.vs[node]["angle"] = G.node[node]["angle"]
+#        g.add_edges(G.edges())
+#
+#    g.save("hyperbolicModel"+"rand"+str(random.randrange(1, 100))+".gml")
 
 def wrong_addMiddleNode(G, node1, node2, id):
     """Adds a new node between to nodes."""
@@ -117,8 +117,6 @@ if __name__=='__main__':
                 dist1 = hyperbolicDistance(G, currentNodeID, midNodeID)
                 dist2 = hyperbolicDistance(G, midNodeID, node)
                 distSum = hyperbolicDistance(G, currentNodeID, node)
-                print "--", dist1
-                print "--", dist2
                 print dist1+dist2, "==", distSum
 
             else:
@@ -127,4 +125,4 @@ if __name__=='__main__':
         currentNodeID = midNodeID+1
         
     print len(G.nodes())
-    saveGML(G)
+ #   saveGML(G)
