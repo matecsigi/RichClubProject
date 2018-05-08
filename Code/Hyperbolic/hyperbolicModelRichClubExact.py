4e7f55f15156c06a731d6693ddac1497e5d35615
EC2 Default User <ec2-user@ip-172-31-19-73.ec2.internal>
2018-05-08 13:12:14 +0000

    Results for the exact model

--- Code/Hyperbolic/hyperbolicModelRichClubExact.py
+++ Code/Hyperbolic/hyperbolicModelRichClubExact.py
@@ -22,7 +22,8 @@ def hyperbolicDistance(G, node1, node2, beta=1):
     r2 = G.node[node2]["radius"]
     delta = abs(angle1-angle2)
     #return abs(r1+r2+math.log(sin(delta/float(2))))
-    return acosh(cosh(r1)*cosh(r2)-sinh(r1)*sinh(r2)*cos(delta))
+    dist = acosh(cosh(r1)*cosh(r2)-sinh(r1)*sinh(r2)*cos(delta))
+    return dist.real
 
 def addMiddleNode(G, node1, node2, id):
     """Add a new node between two nodes"""
@@ -31,20 +32,46 @@ def addMiddleNode(G, node1, node2, id):
     r1 = G.node[node1]["radius"]
     r2 = G.node[node2]["radius"]
 
-    radius = numpy.linspace(float(r1), float(r2), num=10)
+    radiusesArray = numpy.linspace(float(r1), float(r2), num=10)
     if(abs(angle1-angle2) < math.pi):
-        angle =numpy.linspace(min(angle1, angle2), max(angle1, angle2), num=10)
+        anglesArray =numpy.linspace(min(angle1, angle2), max(angle1, angle2), num=10)
     else:
-        angle = numpy.append(numpy.linspace(max(angle1,angle2), 2*math.pi, num=10), numpy.linspace(0, min(angle1,angle2), num=10))
-    
-    print "radiusArray=", radius
-    print "angleArray=", angle
+        anglesArray = numpy.append(numpy.linspace(max(angle1,angle2), 2*math.pi, num=10), numpy.linspace(0, min(angle1,angle2), num=10))
 
+    radiuses = radiusesArray.tolist()
+    angles = anglesArray.tolist()
+    
+    fitnessMax = 0
+    rMax = radiuses[0]
+    angleMax = angles[0]
+    for r in radiuses:
+        for angle in angles:
+            tmpG = G
+            tmpG.add_node(id)
+            tmpG.node[id]["radius"] = r
+            tmpG.node[id]["angle"] = angle
+            fitness = fitnessFunction(tmpG, id, node1, node2)
+            if(fitness > fitnessMax):
+                fitnessMax = fitness
+                rMax = r
+                angleMax = angle
+    G.add_node(id)
+    G.node[id]["radius"] = rMax
+    G.node[id]["angle"] = angleMax
+
+    # print "angle1=", angle1, " angle2=", angle2, " angle=", angleMax
+    # print "radius1=", r1, " radius2=", r2, " radius=", rMax
+
+def fitnessFunction(G, nodeMid, node1, node2):
+    dist1 = hyperbolicDistance(G, node1, nodeMid)
+    dist2 = hyperbolicDistance(G, nodeMid, node2)
+    fitness = 10/(abs(dist1-dist2))+1/(dist1+dist2)
+    return fitness
     
 def addNode(G, id):
     """Adds a node to the network assigning coordinates to it."""
     G.add_node(id)
-    G.node[id]["radius"] = hyperbolicRadius(t+1)
+    G.node[id]["radius"] = radiuses[t]
     G.node[id]["angle"] = random.uniform(0, 2*math.pi)
 
 def kClosest(G, id, k):
@@ -75,18 +102,30 @@ def saveRichClub(G):
         print "new try"
         saveRichClub(G)
 
+def generateRadiuses(N, R):
+    radiusList = []
+    for i in range(0, N):
+        radiusList.append(hyperbolicRadius(R))
+    radiusList.sort()
+    return radiusList
+
 if __name__=='__main__':
 
     N = int(sys.argv[1])
     limitDistance = float(sys.argv[2])
+    R = 10
     k = 3
+
+    radiuses = generateRadiuses(N, R)
 
     G = nx.Graph()
     currentNodeID = 0
 
-    for t in range(1, N):
-        addNode(G, currentNodeID)
+    for t in range(0, N):
+        print t
         
+        addNode(G, currentNodeID)
+
         midNodeID = currentNodeID
         for node in kClosest(G, currentNodeID, k):
             if hyperbolicDistance(G, currentNodeID, node) >= limitDistance:
@@ -95,6 +134,11 @@ if __name__=='__main__':
                 G.add_edge(currentNodeID, midNodeID)
                 G.add_edge(midNodeID, node)
 
+                # dist1 = hyperbolicDistance(G, currentNodeID, midNodeID)
+                # dist2 = hyperbolicDistance(G, midNodeID, node)
+                # distSum = hyperbolicDistance(G, currentNodeID, node)
+                # print dist1+dist2, "==", distSum
+
             else:
                 G.add_edge(currentNodeID, node)
 
@@ -102,5 +146,5 @@ if __name__=='__main__':
         
     print "nodes: ",len(G.nodes())
     print "edges: ", G.number_of_edges()
-    # saveGML(G, N, limitDistance)
-    # saveRichClub(G)
+    saveGML(G, N, limitDistance)
+    saveRichClub(G)
